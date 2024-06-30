f98ea25a544ae10ab5c16cb6313327368a55bb83
Yaroslav-Pa <yaroslav.roro@gmail.com>
2024-06-30 22:06:38 +0300

    clear project from no used variable

--- client/src/components/InputComponents/FieldFileInput/FieldFileInput.jsx
+++ client/src/components/InputComponents/FieldFileInput/FieldFileInput.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Field, useField } from 'formik';
+import { useField } from 'formik';
 
 const FieldFileInput = ({ classes, name }) => {
   const { fileUploadContainer, labelClass, fileNameClass, fileInput } = classes;
 
-  const [{ value, ...field }, meta, helpers] = useField(name);
+  const [{ value, ...field }, helpers] = useField(name);
 
   const getFileName = () => {
     if (value) {

a3b7ddecc31aefc3e662d27ab9b9c6dfca6dc919
Yaroslav-Pa <yaroslav.roro@gmail.com>
2024-07-05 16:07:00 +0300

    unsub from notification and fix undo migration

--- client/src/utils/redirectFunctions.js
+++ client/src/utils/redirectFunctions.js
@@ -1,7 +1,13 @@
 import CONSTANTS from '../constants';
+import store from '../store';
+import { clearChatDataToInitial } from '../store/slices/chatSlice';
+import { clearUserStore } from '../store/slices/userSlice';
 
 export const redirectToLogin = (history) => {
+  const dispatch = store.dispatch;
   window.localStorage.removeItem(CONSTANTS.ACCESS_TOKEN);
+  dispatch(clearUserStore());
+  dispatch(clearChatDataToInitial());
   history.replace({
     pathname: '/login',
     search: '?isTokenExpired=true',
